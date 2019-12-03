104ef503b30ee283ee4791bcd9f599ad09615d5c
raluiz <45321496+raluiz@users.noreply.github.com>
2019-12-03 14:27:26 -0300

    Updated 03/12/19
    Improvement in API's functionality

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ function getURL(url) {
 	return request.responseText;
 }
 
+function getData(req) {
+	/*
+	* Request data about user's github repository.
+	* If user doesn't exist, returns null.
+	*/
+	const data = JSON.parse(getURL('https://api.github.com/users/'+ req.params.user + '/repos'));
+	
+	if(data.message === "Not Found") {
+		return null;
+	} else {
+		return data;
+	}
+}
+
+function getByLanguage(data, req) {
+	/*
+	* Get repos that has a specific language.
+	*/
+	
+	var rep = [];
+	
+	if(req.params.language == "C_") {
+		req.params.language = "C#";
+	}
+	for(n in data) {
+		if(data[n].language == req.params.language) {
+			rep.push(data[n]);
+		}
+	}
+	
+	return rep;
+}
 function order(rep) {
 	/*
 	* Order a list
@@ -36,7 +68,7 @@ function order(rep) {
 
 function get_oldest(rep, n) {
 	/*
-	* Get n oldest repos.
+	* Get avatar, title and subtitle from the five oldest rep
 	*/
 	var oldest = [];
 	
@@ -47,48 +79,77 @@ function get_oldest(rep, n) {
 	return oldest;
 }
 
-// Getting Take's github data and ordering C# repos by creation date and acquiring only five of them.
-const data = JSON.parse(getURL('https://api.github.com/users/takenet/repos'));
-
-var rep = [];
-
-for(n in data) {
-	if(data[n].language == "C#") {
-		rep.push(data[n]);
-	}
-}
-
-rep = order(rep);
-rep = get_oldest(rep, 5);
 
 // Actions for each URL.
-app.get('/repositories/', (req, res) => {
+app.get('/repositories/:user', (req, res) => {
 	/*
-	* Returns the data acquired from the chosen URL.
+	* If user exists, sends data about it's repos.
 	*/
-	res.send(getURL('https://api.github.com/users/takenet/repos'));
-});
-
-app.get('/repositories/C_', (req, res) => {
-	/*
-	* Returns data about the five oldest C# repositories.
-	*/
-	res.send(JSON.stringify(rep));
-});
-
-app.get('/repositories/img', (req, res) => {
-	/*
-	* Returns a link to repositories' owner avatar.
-	*/
-	res.send(data[0].owner.avatar_url);
-});
-
-app.get('/repositories/C_/:n', (req, res) => {
-	/*
-	* Returns a C# repository data.
-	*/
-	const n = parseInt(req.params.n);
+	const data = getData(req);
 	
-	res.send(rep[n]);
+	if(data == null) {
+		res.send(null);
+	} else {
+		res.send(data);
+	}
 });
+
+app.get('/repositories/:user/:language', (req, res) => {
+	/*
+	* Returns data about an user's repo that has a specific language.
+	*/
+	const data = getData(req);
+	var rep = [];
+	
+	if(data == null) {
+		res.send("User not found");
+	} else {
+		var rep = getByLanguage(data, req);
+		
+		if(rep.length > 0) {
+			res.send(JSON.stringify(rep));
+		} else {
+			res.send(null);
+		}
+	}
+});
+
+app.get('/repositories/:user/img', (req, res) => {
+	/*
+	* Returns a link to repositories' owner img.
+	*/
+	const data = getData(req);
+	
+	if(data == null) {
+		res.send(null);
+	} else {
+		res.send(data[0].owner.avatar_url);
+	}
+});
+
+app.get('/repositories/:user/:language/:i', (req, res) => {
+	/*
+	* Returns an user's i oldest repos that has a specific language.
+	*/
+	const data = getData(req);
+	var rep = [];
+	
+	if(data == null) {
+		res.send("User not found");
+	} else {
+		const i = parseInt(req.params.i);
+		
+		var rep = getByLanguage(data, req);
+		
+		rep = order(rep);
+		
+		if(rep.length >= i) {
+			rep = get_oldest(rep, i);
+			res.send(JSON.stringify(rep));
+		} else {
+			res.send(null);
+		}
+	}
+});
+
 app.listen(3000, () => console.log('Listening on port 3000'));
